cd2791dde2100ae958499aad4e9612b39b1d1baa
Divya <dvadnere@trueigtech.com>
2026-01-20 18:24:14 +0530

    feat(scores): tracking the scores

--- main.js
+++ main.js
@@ -3,8 +3,16 @@ const input = document.getElementById("task-input");
 const addBtn = document.getElementById("add-button");
 const taskList = document.getElementById("task-list");
 const completedList = document.getElementById("completed-list");
+//let score = document.getElementByClass("score-box");
 //creating a function to append the new tasks to the tasks array
 let tasks = [];
+let scored = 0;
+let savedTasks = localStorage.getItem("tasks");
+
+if (savedTasks) {
+  tasks = JSON.parse(savedTasks);
+}
+
 function addTask() {
   let taskText = input.value;
   if (taskText === "") return;
@@ -16,13 +24,14 @@ function addTask() {
 
   input.value = ""; //clear the input
   console.log(tasks);
-  renderTasks();
+  saveTasks();
+  showTasks();
 }
 // whenever user clicks the button, addTask will be called
 addBtn.addEventListener("click", addTask);
 
-// to render the taskslist on screen
-function renderTasks() {
+// to show the taskslist on screen
+function showTasks() {
   taskList.innerHTML = "";
   completedList.innerHTML = "";
   for (let i = 0; i < tasks.length; i++) {
@@ -43,14 +52,17 @@ function renderTasks() {
 
     checkbox.addEventListener("change", function () {
       tasks[i].completed = checkbox.checked;
-      renderTasks();
+      showTasks();
     });
 
     li.appendChild(checkbox);
     li.appendChild(span);
     li.appendChild(deleteButton);
+
     if (tasks[i].completed) {
       completedList.appendChild(li);
+      scored++;
+      console.log("current score is:", scored);
     } else {
       taskList.appendChild(li);
     }
@@ -60,5 +72,10 @@ function renderTasks() {
 //for deleting any task
 function deleteTask(index) {
   tasks.splice(index, 1);
-  renderTasks();
+  showTasks();
+}
+
+//saving to local storage
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
